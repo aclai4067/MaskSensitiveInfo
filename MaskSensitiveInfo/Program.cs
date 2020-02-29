023c788e2e485cf54e8f65597689527f8ffde836
Ashley Claiborne <languageisaliquid@gmail.com>
2020-02-29 12:58:08 -0600

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me a secret");
+            var secret = Console.ReadLine();
+            var counter = 0;
+
+            StringBuilder maskedSecret = new StringBuilder();
+
+            foreach (var letter in secret)
+            {
+                if (counter < 4)
+                {
+                    maskedSecret.Append(letter.ToString());
+                }
+                else
+                {
+                    maskedSecret.Append("X");
+                }
+                counter += 1;
+            }
+
+            Console.WriteLine(maskedSecret);
+            Console.ReadLine();
+        }
+    }
+}
